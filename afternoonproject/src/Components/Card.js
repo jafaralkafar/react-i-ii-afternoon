2c90d711eae6924dd2165725f7b5c9dcc92c9402
Grant Fox <granttfox@gmail.com>
2019-05-23 10:12:32 -0600

    fixed the bold

--- afternoonproject/src/Components/Card.js
+++ afternoonproject/src/Components/Card.js
@@ -16,9 +16,9 @@ export default function Card(props) {
             <h2 className='inumber'>{`${props.count+1}/${props.people.length}`}</h2>
             <h2 className='iname'>
             {`${props.people[props.count].name.first} ${props.people[props.count].name.last}`}</h2>
-            <p className='iinfo'><b>From: {`${props.people[props.count].city}, ${props.people[props.count].country}`}</b> </p>
-            <p className='iinfo'><b>Job Title: {props.people[props.count].title}</b> </p>
-            <p className='iinfo'><b>Employer: {props.people[props.count].employer}</b> </p>
+            <p className='iinfo'><b>From:</b> {`${props.people[props.count].city}, ${props.people[props.count].country}`}</p>
+            <p className='iinfo'><b>Job Title:</b> {props.people[props.count].title} </p>
+            <p className='iinfo'><b>Employer:</b> {props.people[props.count].employer} </p>
             <br className='iinfo' />
             <p className='iinfo'><b>Favorite Movies:</b></p>
             <ol className='iinfo'>
